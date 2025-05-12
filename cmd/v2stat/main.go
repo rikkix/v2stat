ce8dbb7686bd1d9729b0396d7557db8126fe5cae
Rikki <i@rikki.moe>
2025-05-12 15:50:39 +0800

    use influx db instead

--- cmd/v2stat/main.go
+++ cmd/v2stat/main.go
@@ -1,70 +1,105 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"flag"
-	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	"github.com/jedib0t/go-pretty/table"
-	"github.com/jedib0t/go-pretty/text"
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/influxdata/influxdb-client-go/v2"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 
-	"go.rikki.moe/v2stat"
+	"go.rikki.moe/v2stat/command"
 )
 
 var (
-	flagInterval = flag.Int("interval", 300, "Interval in seconds to record stats")
-	flagDatabase = flag.String("db", "", "Path to SQLite database")
-	flagServer   = flag.String("server", "127.0.0.1:8080", "V2Ray API server address")
-	flagLogLevel = flag.String("log-level", "info", "Log level (debug, info, warn, error, fatal, panic)")
+	flagServerName  = flag.String("name", "", "Name of the server")
+	flagInterval    = flag.Int("interval", 300, "Interval in seconds to record stats")
+	flagInflux      = flag.String("influx", "", "URL to InfluxDB database")
+	flagInfluxToken = flag.String("token", "", "InfluxDB token")
+	flagOrg         = flag.String("org", "", "InfluxDB organization")
+	flagBucket      = flag.String("bucket", "", "InfluxDB bucket")
+	flagServer      = flag.String("server", "127.0.0.1:8080", "V2Ray API server address")
+	flagLogLevel    = flag.String("log-level", "info", "Log level (debug, info, warn, error, fatal, panic)")
 )
-
-var DefaultDBPaths = []string{
-	"v2stat.db",
-	"traffic.db",
-	"/var/lib/v2stat/traffic.db",
-	"/usr/local/share/v2stat/traffic.db",
-	"/opt/apps/v2stat/traffic.db",
-}
 
 var logger *logrus.Logger
 
 func main() {
 	flag.Parse()
-	args := flag.Args()
-
-	if len(args) < 1 {
-		fmt.Println("Usage: v2stat <command> [args]")
-		fmt.Println("Available commands: daemon, query")
-		os.Exit(1)
-	}
 
 	logger = setupLogger(*flagLogLevel)
-	db := setupDatabase(logger, *flagDatabase)
 
-	v2s := v2stat.NewV2Stat(logger, db, nil)
-	defer v2s.Close()
-
-	switch args[0] {
-	case "daemon":
-		conn, err := grpc.NewClient(*flagServer, grpc.WithInsecure())
+	// Use hostname as default server name if not provided
+	servername := *flagServerName
+	if servername == "" {
+		hostname, err := os.Hostname()
 		if err != nil {
-			logger.Fatalf("Failed to dial gRPC server: %v", err)
+			logger.Fatalf("Failed to get hostname: %v", err)
 		}
-		v2s.SetConn(conn)
-		runDaemon(v2s)
-
-	case "query":
-		handleQuery(v2s, args[1:])
-
-	default:
-		fmt.Println("Unknown command:", args[0])
-		fmt.Println("Available commands: daemon, query")
-		os.Exit(1)
+		servername = hostname
 	}
+	logger.Infof("Using server name: %s", servername)
+
+	// Set up gRPC connection to V2Ray API server
+	conn, err := grpc.NewClient(*flagServer, grpc.WithInsecure())
+	if err != nil {
+		logger.Fatalf("Failed to connect to V2Ray API server: %v", err)
+	}
+	defer conn.Close()
+	client := command.NewStatsServiceClient(conn)
+	if client == nil {
+		logger.Fatalf("Failed to create V2Ray API client")
+	}
+
+	// Set up InfluxDB client
+	influxClient := influxdb2.NewClient(*flagInflux, *flagInfluxToken)
+	defer influxClient.Close()
+
+	// Create a new point batch
+	bp := influxClient.WriteAPIBlocking(*flagOrg, *flagBucket)
+
+	ticker := time.NewTicker(time.Duration(*flagInterval) * time.Second)
+	defer ticker.Stop()
+	killsig := make(chan os.Signal, 1)
+	signal.Notify(killsig, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		now := time.Now()
+		stats, err := client.QueryStats(context.Background(), &command.QueryStatsRequest{
+			Reset_: true,
+		})
+		if err != nil {
+			logger.Errorf("Failed to get stats: %v", err)
+			goto LOOP_FINAL
+		}
+
+		// Write stats to InfluxDB
+		for _, stat := range stats.Stat {
+			point := influxdb2.NewPoint(
+				"v2ray_stats",
+				map[string]string{"server": servername, "stat": stat.Name},
+				map[string]interface{}{"value": stat.Value},
+				now,
+			)
+			if err := bp.WritePoint(context.Background(), point); err != nil {
+				logger.Errorf("Failed to write point to InfluxDB: %v", err)
+			}
+		}
+
+LOOP_FINAL:
+		select {
+		case <-ticker.C:
+			continue
+		case sig := <-killsig:
+			logger.Infof("Received signal: %s", sig)
+			return
+		}
+	}
+
 }
 
 func setupLogger(levelStr string) *logrus.Logger {
@@ -75,85 +110,4 @@ func setupLogger(levelStr string) *logrus.Logger {
 	logger := logrus.New()
 	logger.SetLevel(level)
 	return logger
-}
-
-func setupDatabase(logger *logrus.Logger, dbpath string) *sql.DB {
-	if dbpath == "" {
-		for _, path := range DefaultDBPaths {
-			if _, err := os.Stat(path); err == nil {
-				dbpath = path
-				break
-			}
-		}
-		if dbpath == "" {
-			logger.Fatal("No database path provided and no default database found.")
-		}
-	}
-	logger.Infof("Using database: %s", dbpath)
-
-	db, err := sql.Open("sqlite3", dbpath)
-	if err != nil {
-		logger.Fatalf("Failed to open database: %v", err)
-	}
-	return db
-}
-
-func handleQuery(v2s *v2stat.V2Stat, args []string) {
-	if len(args) == 0 {
-		fmt.Println("Usage: v2stat query <connection_name>")
-		fmt.Println("Available connections:")
-		conns, err := v2s.QueryConn()
-		if err != nil {
-			logger.Fatalf("Failed to query connection: %v", err)
-		}
-		for _, c := range conns {
-			fmt.Printf("\t%s\n", c.String())
-		}
-		return
-	}
-
-	connStr := args[0]
-	conn, ok := v2stat.ParseConnInfo(connStr)
-	if !ok {
-		logger.Fatalf("Invalid connection format: %s", connStr)
-	}
-
-	stats, err := v2s.QueryStatsHourly(&conn)
-	if err != nil {
-		logger.Fatalf("Failed to query stats: %v", err)
-	}
-
-	printStatsTable(stats)
-}
-
-func printStatsTable(stats []v2stat.TrafficStat) {
-	tb := table.NewWriter()
-	tb.SetOutputMirror(os.Stdout)
-	tb.AppendHeader(table.Row{"Time", "Downlink", "Uplink"})
-
-	var totalDown, totalUp int64
-
-	for _, stat := range stats {
-		totalDown += stat.Downlink
-		totalUp += stat.Uplink
-		tb.AppendRow(table.Row{stat.Time, sizeToHuman(stat.Downlink), sizeToHuman(stat.Uplink)})
-	}
-
-	tb.AppendFooter(table.Row{"Total", sizeToHuman(totalDown), sizeToHuman(totalUp)})
-	style := table.StyleLight
-	style.Format.Footer = text.FormatDefault
-	tb.SetStyle(style)
-	tb.Render()
-}
-
-func sizeToHuman(size int64) string {
-	units := []string{"B", "KiB", "MiB", "GiB", "TiB"}
-	value := float64(size)
-	for _, unit := range units {
-		if value < 1024 {
-			return fmt.Sprintf("%7.2f %s", value, unit)
-		}
-		value /= 1024
-	}
-	return fmt.Sprintf("%7.2f %s", value, "PiB")
 }
--- go.mod
+++ go.mod
@@ -2,42 +2,26 @@ module go.rikki.moe/v2stat
 
 go 1.24.2
 
-require google.golang.org/grpc v1.71.1
-
 require (
-	github.com/adrg/xdg v0.5.3 // indirect
-	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
-	github.com/go-openapi/errors v0.22.0 // indirect
-	github.com/go-openapi/strfmt v0.23.0 // indirect
-	github.com/gofiber/fiber/v2 v2.52.6 // indirect
-	github.com/gofiber/template v1.8.3 // indirect
-	github.com/gofiber/template/html/v2 v2.1.3 // indirect
-	github.com/gofiber/utils v1.1.0 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/jedib0t/go-pretty v4.3.0+incompatible // indirect
-	github.com/jedib0t/go-pretty/v6 v6.6.7 // indirect
-	github.com/klauspost/compress v1.17.9 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/mattn/go-sqlite3 v1.14.27 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/oklog/ulid v1.3.1 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.51.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	go.mongodb.org/mongo-driver v1.14.0 // indirect
+	github.com/influxdata/influxdb-client-go/v2 v2.14.0
+	google.golang.org/grpc v1.71.1
 )
 
 require (
-	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/adrg/xdg v0.5.3 // indirect
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/oapi-codegen/runtime v1.0.0 // indirect
+)
+
+require (
+	github.com/sirupsen/logrus v1.9.3
 	github.com/v2fly/v2ray-core/v5 v5.30.0
 	golang.org/x/net v0.38.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
-	google.golang.org/protobuf v1.36.5 // indirect
+	google.golang.org/protobuf v1.36.5
 )
